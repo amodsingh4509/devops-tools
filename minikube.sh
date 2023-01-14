1e26d86455315ee0d1474fc7b440f90a8ae52cf4
Amod <amodkumar968@gmail.com>
2023-01-14 18:32:29 +0530

    initital commit

---
+++ minikube.sh
@@ -0,0 +1,34 @@
+#docker installation 
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y 
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+sudo usermod -aG docker $USER && newgrp docker
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl
+
+# minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+minikube start --driver=docker
+
+
+
